b0e5176ee2fcf955058b0365dc188693c19951f8
PRIYANSHU KUMAR YADAV <priyanshu322006@gmail.com>
2026-02-02 00:54:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ const bgMusic = document.getElementById("bgMusic");
 const enterBtn = document.getElementById("enterBtn");
 const content = document.getElementById("content");
 const intro = document.querySelector(".intro");
+const voiceNote = document.getElementById("voiceNote");
+
+bgMusic.volume = 0.25;
 
 enterBtn.addEventListener("click", () => {
   bgMusic.play().catch(() => {});
   intro.style.display = "none";
   content.classList.remove("hidden");
 });
+
+voiceNote.addEventListener("play", () => {
+  bgMusic.pause();
+});
+
+voiceNote.addEventListener("ended", () => {
+  bgMusic.play().catch(() => {});
+});
+
